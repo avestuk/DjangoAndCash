b84d18fb1e46f5ee2bc1a085cc98abde56144b6e
Alex <avestuk@gmail.com>
2017-09-16 16:47:11 +0100

    Use generic views: Less code is better

--- tutorialsite/polls/models.py
+++ tutorialsite/polls/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -12,6 +13,7 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -19,6 +21,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
-
-
